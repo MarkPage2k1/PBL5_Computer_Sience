bcf6afdc43df9df13831a653263834358b0b960d
Trung Quân <87590541+TrQuan17@users.noreply.github.com>
2022-06-21 23:40:48 +0700

    Delete PBL5_v1 directory

--- PBL5_v1/Login/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from Login.views import login_view, reghister_view
-urlpatterns = [
-    path('', login_view, name = 'login'),
-    path('reghister', reghister_view, name = 'reghister')
-]
